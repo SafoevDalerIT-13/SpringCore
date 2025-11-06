47b3871da75889154f815fe8d52c0c27ad38376f
Далер <dsafoev40@gmail.com>
2025-11-06 19:26:39 +0500

    Create ConfigurationProperties.java

---
+++ org.example/ConfigurationProperties.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@EnableAspectJAutoProxy
+@PropertySource("classpath:application.properties")
+public class ConfigurationProperties {
+
+}
